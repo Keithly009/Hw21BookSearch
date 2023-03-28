61f75a6e417adc9b25766d08f8c317e30db28c76
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2023-03-27 21:22:24 -0700

    SignupForm, client-Side

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,8 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
-
-import { loginUser } from "../utils/API";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/API";
 import Auth from "../utils/auth";
 
 const LoginForm = () => {
@@ -35,13 +35,13 @@ const LoginForm = () => {
     }
 
     try {
-      const {data} = await login ({ 
-        variables: {...userFormData}, 
-      }); 
-      console.log(data); 
-      Auth.login(data.login.token); 
-    } catch (e) { 
-      console.error(e); 
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+      console.log(data);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
     }
 
     setUserFormData({
