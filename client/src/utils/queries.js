8bae47fea4cf5c577f94ff507af2fef66f7cd48c
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2023-03-28 03:47:31 -0700

    debugging: src/utils

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,19 @@
-import { gql } from '@apollo/client'; 
+import { gql } from "@apollo/client";
 
-export const QUERY_ME = gql 
-` { 
-    me { 
-        _id 
-        username 
-        email
-        savedBooks { 
-            bookId
-            authors
-            image
-            description
-            title
-            link
-        }
+export const QUERY_ME = gql
+`{
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
     }
-}`
+  }
+`;
