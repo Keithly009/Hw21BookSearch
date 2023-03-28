1e1b856049c850b07f9d2b29ce34456188010ab1
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2023-03-27 21:43:12 -0700

    Another Commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express')
 const { typeDefs, resolvers } = require('./schemas')
 const { authMiddleware } = require('./utils/auth')
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-})
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,  
+  context: authMiddleware
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
