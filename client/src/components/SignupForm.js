9c90917954fe8fcf8575239cef69d5ec9398108e
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2023-03-27 22:12:31 -0700

    added mutations

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client'; 
 import {ADD_USER} from '@apollo/client'; 
 import Auth from '../utils/auth';
 
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client'; 
+
+export const LOGIN_USER = gql
+`mutation login ($email: String!, $password: String!) {
+    login(email: $email, password: $password) { 
+        token 
+        user {
+            _id 
+            username
+        }
+    }
+}`; 
+
+export const ADD_USER = gql 
+`mutatioon addUser($username: string!, $email: String!, $password: String!) { 
+    addUser(username: $username, email: password: $password) { 
+        token 
+        user { 
+            _id
+            username 
+        }
+    }
+}`; 
+
+export const SAVE_BOOK = gql 
+`mutation saveBook($bookData: BookInput!) { 
+    saveBook(bookData: $bookData) { 
+        _id 
+        username 
+        email 
+        savedBooks { 
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
+}`; 
+
+export const REMOVE_BOOK = gql 
+` mutation removeBook($bookid: ID!) { 
+    removeBook(bookId: $bookId) { 
+        _id
+        username
+        email
+        savedBooks { 
+            bookId
+            authors
+            image
+            description
+            title 
+            link
+        }
+    }
+}`;
